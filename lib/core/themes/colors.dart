101244e7f820d0204243e86b11fe77e468aa9b66
rowan <90569337+rowanibrahem@users.noreply.github.com>
2024-10-08 22:56:30 +0200

    شيلي الالوان الاساسية

--- lib/core/themes/colors.dart
+++ lib/core/themes/colors.dart
@@ -8,7 +8,7 @@ abstract class ColorsApp{
   static Color green = const Color(0xff25D366);
   static Color lightGreen = const Color(0xffDCF8C6);
   static Color blue = const Color(0xff34B7F1);
-  static Color grey = const Color(0xffECE5DD);
+  static Color grey = const Color(0xffECE5DD); // امسحي الالوان الاساسية لو دي درجة لون اساسي 
 
 
 
